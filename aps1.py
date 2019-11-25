965d760ebde191056d63ba2cd51897e161657f70
Sabrina Machado Bezerra <sabrinamb@al.insper.edu.br>
2019-11-24 21:13:29 -0300

    Add files via upload

--- aps1.py
+++ aps1.py
@@ -2,21 +2,27 @@ from flask import Flask, render_template, request
 import flask
 from flask_restful import Api, Resource
 import json
-from pprint import pprint as print
+import pymongo
+import os
 
+ipBD = os.environ['ipBD']
 
+client = pymongo.MongoClient("mongodb://sabrina:mongolito@{0}/tarefas".format(ipBD)) # defaults to port 27017
 
-def add_tarefa(tarefa):
+db = client.tarefas
+
+tarefas = db.tarefas
+
+dictglobal = {}
+global id_adicionado
+id_adicionado = 0
+def add_tarefa(tarefa, idee):
 	t = tarefa.tempo
 	d = tarefa.dificuldade
 	dic = {"tempo": t, "dificuldade": d}
-	if dictglobal == {}:
-		id_adicionado = 0
-	else:
-		id_adicionado = (list(dictglobal.keys())[-1])+1
-	d = {id_adicionado : dic}
-	dictglobal.update(d)
-	return id_adicionado
+	mydict = {"_id" : idee, "tempo": t, "dificuldade": d}
+	tarefas.insert_one(mydict)
+	return idee
 
 def update_tarefa(tarefa, id_tarefa):
 	t = tarefa.tempo
@@ -26,13 +32,16 @@ def update_tarefa(tarefa, id_tarefa):
 		id_adicionado = 0
 	else:
 		id_adicionado = (list(dictglobal.keys())[id_tarefa])
-	d = {id_adicionado : dic}
-	dictglobal.update(d)
+	myquery = { "_id": id_tarefa }
+	newvalues = { "$set": dic }
+
+	tarefas.update_one(myquery, newvalues)
 	return id_adicionado
 
-
 def remove_tarefa(id_tarefa):
-	del dictglobal[id_tarefa]
+	myquery = { "_id": id_tarefa }
+
+	tarefas.delete_one(myquery)
 
 
 # • Crie uma classe denominada Tarefas. Defina pelo menos uns 2 atributos.
@@ -48,8 +57,6 @@ class Tarefas():
 # 		um id autoincremental de tarefas.
 
 
-dictglobal = {}
-
 
 
 # • Crie um webserver em Python usando o Flask.
@@ -64,17 +71,25 @@ app = flask.Flask('your_flask_env')
 # – /Tarefa/ - POST: Adiciona uma tarefa.
 
 @app.route('/Tarefa', methods=['GET', 'POST'])
+# @app.route("/<path:path>")
 def Tarefa():
+	global id_adicionado
+	id_adicionado = 0
 	if flask.request.method == 'POST':
 		# adiciona uma tarefa
-		add_tarefa(Tarefas(request.get_json()['tempo'], request.get_json()['dificuldade']))
+		vai = []
+		for x in db.tarefas.find().sort("_id",-1):
+			vai.append(x)
+			break
+		add_tarefa(Tarefas(request.get_json()['tempo'], request.get_json()['dificuldade']), int(vai[0]['_id']) + 1)
+		id_adicionado += 1
 		return ("ok", 200)
 	else:
 		# lista todas as tarefas do dicionario
-		if dictglobal != {}:
-			return(str(dictglobal), 200)
-		else:
-			return("dict vazio", 400)
+		vai = []
+		for x in tarefas.find():
+			vai.append(x)
+		return(str(vai), 200)
 
 
 
@@ -92,7 +107,10 @@ def TarefaID(id_task):
 		update_tarefa(Tarefas(request.get_json()['tempo'], request.get_json()['dificuldade']), id_task)
 		return ("ok", 200) #dictglobal[numero]
 	else:
-		return """O tempo estimado para a tarefa {0} é de {1} hora(s), e sua dificuldade foi avaliada como {2}; """.format(id_task, dictglobal[id_task]['tempo'], dictglobal[id_task]['dificuldade'])#render_template("index.html")#.format(dictglobal[numero])
+		vai = []
+		for x in tarefas.find({ "_id": id_task}):
+			vai.append(x)
+		return(str(vai), 200)#render_template("index.html")#.format(dictglobal[numero])
 
 
 # – /healthcheck/ - Retorna o código 200 sem texto.
@@ -100,6 +118,28 @@ def TarefaID(id_task):
 @app.route('/healthcheck')
 def healthy():
 	return ("", 200)
+
+
+
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def catch_all(path, methods=['GET', 'PUT', 'DELETE', 'POST']):
+# 	if flask.request.method == 'DELETE':
+# 		remove_tarefa(id_task)
+# 		return ("ok", 200)
+# 	elif flask.request.method == 'PUT':
+# 		print(request.get_json())
+# 		update_tarefa(Tarefas(request.get_json()['tempo'], request.get_json()['dificuldade']), id_task)
+# 	elif flask.request.method == 'POST':
+# 		add_tarefa(Tarefas(request.get_json()['tempo'], request.get_json()['dificuldade']))
+# 		return ("ok", 200)
+# 	elif flask.request.method == 'GET':
+# 		if dictglobal != {}:
+# 			return(str(dictglobal), 200)
+# 		else:
+# 			return("dict vazio", 400)
+# 	else:
+#     	return 'You want path: %s' % path
 
 
 # • Teste o serviço acima em localhost, olhando como o dicionário é alterado. Atenção na hora em que serializar
--- script_webserver.sh
+++ script_webserver.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-export PATH=/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 sudo apt-get update
 sudo apt install python3-pip -y
-pip3 install flask flask_restful
-python3 /home/ubuntu/Cloud-APSs/aps1.py
+pip3 install flask flask_restful requests pymongo
+python3 aps1.py
