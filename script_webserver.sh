ccb4d70ce99d0fb3af0ca87af4ae50189468ca39
Sabrina Machado Bezerra <sbrnmchd@gmail.com>
2019-11-25 14:17:51 -0300

    Update script_webserver.sh

--- script_webserver.sh
+++ script_webserver.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+export PATH=/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 sudo apt-get update
 sudo apt install python3-pip -y
 pip3 install flask flask_restful requests pymongo
